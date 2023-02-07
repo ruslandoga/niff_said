5e2d3fb1fc24ec019fbea7838d1c215afadefa12
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-02-07 18:57:29 +0700

    init

---
+++ lib/niff_said.ex
@@ -0,0 +1,18 @@
+defmodule NiffSaid do
+  @moduledoc """
+  Documentation for `NiffSaid`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NiffSaid.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/niff_said_test.exs
@@ -0,0 +1,8 @@
+defmodule NiffSaidTest do
+  use ExUnit.Case
+  doctest NiffSaid
+
+  test "greets the world" do
+    assert NiffSaid.hello() == :world
+  end
+end
